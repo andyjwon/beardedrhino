83c9df04de9cd3cb0e4181a08bb7ac5655b3675f
Andrew Won <andyjwon@gmail.com>
2014-06-08 18:35:42 -0700

    Refactor javascript line execution to not close context on first run, create resource directory with placeholder html

--- src/main/java/org/absolutetech/beardedrhino/App.java
+++ src/main/java/org/absolutetech/beardedrhino/App.java
@@ -3,17 +3,21 @@ package org.absolutetech.beardedrhino;
 import org.absolutetech.beardedrhino.util.JavaScriptRunner;
 
 /**
- * Hello world!
- *
+ * Hello world... in JavaScript!
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         JavaScriptRunner jsRunner = new JavaScriptRunner();
-        String js = "var s = 'Hello, Rhino!'; s;";
+        String jsJQueryTest = "var ver = $.fn.jquery; ver;";
+        String jsHelloWorld = "var s = 'Hello, Rhino!'; s;";
+
         jsRunner.initializeEnvJsAndJQuery();
-        jsRunner.executeJavaScriptLine(js);
-        System.out.println( "Hello World!" );
+
+        jsRunner.executeJavaScriptLine(jsJQueryTest);
+        String helloWorldResult = jsRunner.executeJavaScriptLine(jsHelloWorld);
+        System.out.println( helloWorldResult );
+        jsRunner.exitContext();
     }
 }
--- src/main/java/org/absolutetech/beardedrhino/util/JavaScriptRunner.java
+++ src/main/java/org/absolutetech/beardedrhino/util/JavaScriptRunner.java
@@ -8,10 +8,15 @@ import org.mozilla.javascript.tools.shell.Main;
 
 public class JavaScriptRunner {
 
+    // Minimal optimization for runtime performance
+    private static final Integer RHINO_CONTEXT_OPTIMIZATION_LEVEL = 1;
+    private static final Integer RHINO_LANGUAGE_VERSION = Context.VERSION_1_5;
+
+    private static final String ENV_JS_FILEPATH = "../src/main/resources/javascript/vendor/env.rhino.1.2.js";
+    private static final String JQUERY_FILEPATH = "../src/main/resources/javascript/vendor/jquery-1.11.1.js";
+
     private Integer lineNumber;
-
     private Context cx;
-
     private Global scope;
 
     public JavaScriptRunner() {
@@ -20,29 +25,40 @@ public class JavaScriptRunner {
         cx = ContextFactory.getGlobal().enterContext();
         scope = new Global(cx);
 
-        cx.setOptimizationLevel(-1);
-        cx.setLanguageVersion(Context.VERSION_1_5);
+        cx.setOptimizationLevel(RHINO_CONTEXT_OPTIMIZATION_LEVEL);
+        cx.setLanguageVersion(RHINO_LANGUAGE_VERSION);
     }
 
     public void initializeEnvJsAndJQuery() {
-        Main.processFile(cx, scope, "../src/main/javascript/vendor/env.rhino.1.2.js");
-        Main.processFile(cx, scope, "../src/main/javascript/vendor/jquery-1.11.1.js");
+        initializeJavaScriptFile(ENV_JS_FILEPATH);
+        initializeJavaScriptFile(JQUERY_FILEPATH);
     }
 
     public String executeJavaScriptLine(String javaScriptLine) {
         try {
-            Object obj2 = cx.evaluateString(scope, "var ver = $.fn.jquery; ver;", "versioncheck", lineNumber++, null);
-            System.out.println("jQuery version: " + obj2.toString());
+            Object jQueryVersion = cx.evaluateString(scope, "var ver = $.fn.jquery; ver;", "versioncheck", lineNumber++, null);
+            System.out.println("jQuery version: " + jQueryVersion.toString());
             Object obj = cx.evaluateString(scope, javaScriptLine, "RunLine", lineNumber++, null);
             System.out.println("Object: " + obj.toString());
             return obj.toString();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            Context.exit();
         }
 
         return null;
+    }
+
+    public Boolean exitContext() {
+        if (Context.getCurrentContext() == null) {
+            return false;
+        }
+
+        Context.exit();
+        return true;
+    }
+
+    private void initializeJavaScriptFile(String path) {
+        Main.processFile(cx, scope, path);
     }
 
 }
